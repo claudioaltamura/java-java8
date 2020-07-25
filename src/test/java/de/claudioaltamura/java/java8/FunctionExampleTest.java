4542cc438054912df597f53d3244cfdfacd868ec
claudioaltamura <claudio@claudioaltamura.de>
2020-07-25 12:54:35 +0200

    new: inital commit

---
+++ src/test/java/de/claudioaltamura/java/java8/FunctionExampleTest.java
@@ -0,0 +1,27 @@
+package de.claudioaltamura.java.java8;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionExampleTest {
+
+  @Test
+  void andThen() {
+    Function<String, Integer> toInteger = Integer::valueOf;
+    Function<String, String> backToString = toInteger.andThen(String::valueOf);
+
+    assertThat(backToString.apply("123")).isEqualTo("123");
+  }
+
+  @Test
+  void compose() {
+    Function<String, Integer> toInteger = Integer::valueOf;
+    Function<String, String> addOne = (s) -> s + "1";
+    Function<String, Integer> composed = toInteger.compose(addOne);
+
+    assertThat(composed.apply("123")).isEqualTo(1231);
+  }
+}
