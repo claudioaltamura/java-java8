303ccf9ac9e4c592243823fa778b02d50a4d157b
claudioaltamura <ich@claudioaltamura.de>
2020-09-09 16:45:22 +0200

    new: added comparator example

---
+++ src/test/java/de/claudioaltamura/java/java8/ComparatorTest.java
@@ -0,0 +1,20 @@
+package de.claudioaltamura.java.java8;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Comparator;
+import org.junit.jupiter.api.Test;
+
+class ComparatorTest {
+
+  @Test
+  void compare() {
+
+    Comparator<Person> comparator = (p1, p2) -> p1.firstName.compareTo(p2.firstName);
+
+    Person p1 = new Person("Peter", "Parker");
+    Person p2 = new Person("Bruce", "Wayne");
+
+    assertThat(comparator.compare(p1, p2)).isGreaterThan(0);
+  }
+}
