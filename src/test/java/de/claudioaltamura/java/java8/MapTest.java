8c50c7a7ff7c08024c197a4b7ed9f4c49b40d12c
claudioaltamura <ich@claudioaltamura.de>
2021-08-09 20:56:04 +0200

    new: computeIfAbsent example

--- src/test/java/de/claudioaltamura/java/java8/MapTest.java
+++ src/test/java/de/claudioaltamura/java/java8/MapTest.java
@@ -2,18 +2,29 @@ package de.claudioaltamura.java.java8;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.UUID;
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 
 class MapTest {
 
-	@Test
-	void putIfAbsent() {
-//		V v = map.get(key);
-//		if (v == null)
-//			v = map.put(key, value);
+  @Test
+  void putIfAbsent() {
+    //		V v = map.get(key);
+    //		if (v == null)
+    //			v = map.put(key, value);
 
-		assertThat(CountryFood.food().putIfAbsent("Italy", "Pizza")).isEqualTo("Pizza");
+    assertThat(CountryFood.food().putIfAbsent("Italy", "Pizza")).isEqualTo("Pizza");
 
-		assertThat(CountryFood.food().putIfAbsent("Belgian", " Pomme frites")).isNull();
-	}
+    assertThat(CountryFood.food().putIfAbsent("Belgian", " Pommes frites")).isNull();
+  }
+
+  @Test
+  void computeIfAbsent() {
+    Function<? super String, String> wurstFunction =
+        wurst -> wurst + "-" + UUID.randomUUID();
+    assertThat(CountryFood.food().computeIfAbsent("wiener", wurstFunction))
+        .isNotNull()
+        .startsWith("wiener");
+  }
 }
