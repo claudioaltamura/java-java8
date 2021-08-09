48652bb44efd8cd90b75227fe51e651d1532458d
claudioaltamura <ich@claudioaltamura.de>
2021-08-09 20:55:05 +0200

    chore: formatting

--- src/main/java/de/claudioaltamura/java/java8/CountryFood.java
+++ src/main/java/de/claudioaltamura/java/java8/CountryFood.java
@@ -5,13 +5,12 @@ import java.util.Map;
 
 public class CountryFood {
 
-	private CountryFood() {}
+  private CountryFood() {}
 
-	public static Map<String, String> food() {
-		final Map<String,String> food = new HashMap<>();
-		food.put("Italy", "Pizza");
-		food.put("Spain", "Paella");
-		return food;
-	}
-
+  public static Map<String, String> food() {
+    final Map<String, String> food = new HashMap<>();
+    food.put("Italy", "Pizza");
+    food.put("Spain", "Paella");
+    return food;
+  }
 }
