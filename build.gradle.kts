a11518de8eb2793f078fb889f5ba0865a54c7a43
claudioaltamura <ich@claudioaltamura.de>
2022-02-12 18:05:00 +0100

    chore: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     java
     eclipse
     idea
-    id("com.diffplug.spotless") version "5.14.2"
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.diffplug.spotless") version "6.2.2"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 repositories {
@@ -13,10 +13,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 java {
